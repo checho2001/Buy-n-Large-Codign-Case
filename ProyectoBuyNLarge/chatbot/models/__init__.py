12c374e68653022d2c100092e8a22f1e1085f95b
Sergio Ortiz <saortizc@unbosque.edu.co>
2025-02-23 11:39:23 -0500

    Changes in the main interface

--- ProyectoBuyNLarge/chatbot/models/__init__.py
+++ ProyectoBuyNLarge/chatbot/models/__init__.py
@@ -1,3 +1,2 @@
 from .chat_session import ChatSession
 from .chat_message import ChatMessage
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import Home from './pages/Home';
+import Home from './pages/home'; // Asegúrate de que la ruta de importación sea correcta
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* Redirige la ruta raíz a /login */}
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Navigate to="/login" replace />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
