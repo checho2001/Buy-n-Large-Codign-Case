5d777792f6e972221c34b41d69ef74d42f561d3a
Sergio Ortiz <saortizc@unbosque.edu.co>
2025-02-23 20:23:59 -0500

    Cambios finales

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
-import Home from './pages/Home'; 
+import Home from './pages/home'; 
 import Dashboard from './pages/Dashboard';
 
 function App() {
